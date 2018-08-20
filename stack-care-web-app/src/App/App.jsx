92353ed87fb5f43f0fd644451521e337cb1d6c51
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2018-08-21 02:04:12 +0530

    login page

---
+++ stack-care-web-app/src/App/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <Router history={history}>
+                <div className="h100">
+                    <PrivateRoute exact path="/" component={HomePage} />
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/register" component={RegisterPage} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export { connectedApp as App }; 
---
+++ stack-care-web-app/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { store } from './_helpers';
+import { App } from './App';
+import './index.css'
+
+const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+      primary: {
+        main: "#000000"
+      },
+      secondary: {
+        main: "#008b8b",
+      }
+    }
+  });
+
+render(
+    <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
