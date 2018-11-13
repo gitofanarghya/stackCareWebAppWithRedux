2be3f093906ddc724f0800d05f7ecee6b5a065a5
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2018-11-13 09:09:14 +0530

    refresh middleware

--- stack-care-web-app/src/_helpers/tokenMiddleware.js
+++ stack-care-web-app/src/_helpers/tokenMiddleware.js
@@ -22,7 +22,7 @@ const requestOptions = {
 const checkTokenExpirationMiddleware = store => next => action => {
     if(action.type === INVALID_ACCESS_TOKEN) {
         store.dispatch(request())
-        fetch(`https://care-api-staging.appspot.com/oauth2/tokens`, requestOptions)
+        fetch(`https://care-api-prod.appspot.com/oauth2/tokens`, requestOptions)
             .then(response => response.json())
             .then(user => {
                 if (user.access_token) {
